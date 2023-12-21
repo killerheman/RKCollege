1862635b2083030e16b8fe2f831506cba6905f3c
⚔PRAKASH⚔ <sprakashmaurya999jnp@gmail.com>
2023-12-21 17:16:58 +0530

    upload file

--- resources/views/home/naac/accreditation.blade.php
+++ resources/views/home/naac/accreditation.blade.php
@@ -11,8 +11,8 @@
         </header>
         <div class="page-content">
             <div class="row page-row">
-               <img src="{{ asset('/uploads/2018/02/NAAC2-001-min.jpg') }}" alt="image" style="height: 100%; width:100%; padding-bottom:10px;"><br>
-               <img src="{{ asset('/uploads/2018/02/NAAC3-001-min.jpg') }}" alt="image" style="height: 100%; width:100%">
+               <img src="{{ asset('/uploads/uploads/2018/02/NAAC2-001-min.jpg') }}" alt="image" style="height: 100%; width:100%; padding-bottom:10px;"><br>
+               <img src="{{ asset('/uploads/uploads/2018/02/NAAC3-001-min.jpg') }}" alt="image" style="height: 100%; width:100%">
                 <!--//page-content-->
             </div>
             <!--//page-row-->
