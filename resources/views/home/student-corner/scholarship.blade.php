d14f0a1a74d4e2aea93e46e99b77355d66670938
manish_kumar <manish.prasad@devdigital.com>
2024-01-06 01:36:19 +0530

    other menu

--- resources/views/home/student-corner/scholarship.blade.php
+++ resources/views/home/student-corner/scholarship.blade.php
@@ -13,9 +13,9 @@
             <div class="row page-row mb-5">
 
                 <ol>
-                    <li>1.	Stipend for SC/ST/OBC/Minorities/Physically Handicapped by Govt. of Bihar.</li>
-                    <li>2.	Merit Scholarship by Govt. of Bihar</li>
-                    <li>3.	UGC Sponsored Scholarship.</li>
+                    <li>Stipend for SC/ST/OBC/Minorities/Physically Handicapped by Govt. of Bihar.</li>
+                    <li>Merit Scholarship by Govt. of Bihar</li>
+                    <li>UGC Sponsored Scholarship.</li>
                     </ol>
             </div>
             <!--//page-row-->
