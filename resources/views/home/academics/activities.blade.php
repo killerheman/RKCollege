99a0a3975aefbe638aff712c08f6b876d4e1c186
manish_kumar <manish.prasad@devdigital.com>
2024-01-06 13:25:43 +0530

    academic menu

--- resources/views/home/academics/activities.blade.php
+++ resources/views/home/academics/activities.blade.php
@@ -4,15 +4,87 @@
     <div class="page-wrapper">
 
         <header class="page-heading clearfix">
-            <div class="text-effect" data-content="About College">
-                <span>Academic activities</span>
+            <div class="text-effect" data-content="Academic Activities">
+                <span>Academic Activities</span>
             </div>
             <!--//breadcrumbs-->
         </header>
         <div class="page-content">
             <div class="row page-row mb-5">
-               <h4 class="text-center mt-5">Content to be uploaded soon...</h4>
-                <!--//page-content-->
+
+
+                <div class="fusion-text fusion-text-1">
+                    <h5 class="title-heading-left" data-fontsize="20" data-lineheight="33.4px"><span
+                            style="color: #c7561e;">ACADEMIC ACTIVITIES ARE:</span></h5>
+                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">a) Group discussion<br> b)
+                        Subject oriented mock tests<br> c) Subject presentations by the students<br> d) Lectures / Seminars
+                        / Workshops are organized at the following levels:</h5>
+                    <ul>
+                        <li>
+                            <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                data-lineheight="33.4px" class="fusion-responsive-typography-calculated">At College Level
+                            </h5>
+                        </li>
+                        <li>
+                            <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                data-lineheight="33.4px" class="fusion-responsive-typography-calculated">At Department Level
+                            </h5>
+                        </li>
+                        <li>
+                            <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                data-lineheight="33.4px" class="fusion-responsive-typography-calculated">At different Forums
+                                viz. SHODH, Stree Vimarsh &amp; Forum for Academic Advancement.</h5>
+                        </li>
+                        <li>
+                            <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                data-lineheight="33.4px" class="fusion-responsive-typography-calculated">At various
+                                students’ Forums such as:</h5>
+                            <ol>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Eco-Voice (Economics)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;SPARS, Voice of Commerce (Commerce)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Samajiki (Sociology)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Republica (Political Science)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Virasat (History)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Psychogenesis (Psychology)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Laureates (English)</h5>
+                                </li>
+                                <li>
+                                    <h5 data-fontsize="20" style="--fontSize: 20; line-height: 1.67; --minFontSize: 20;"
+                                        data-lineheight="33.4px" class="fusion-responsive-typography-calculated">
+                                        &nbsp;Sanskriti (AIHC &amp; Archaelogy)</h5>
+                                </li>
+                            </ol>
+                        </li>
+                    </ul>
+                </div>
             </div>
             <!--//page-row-->
         </div>
