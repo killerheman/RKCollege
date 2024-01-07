72d6361d24e084a166ce991d01df1ead1062eb45
manish_kumar <manish.prasad@devdigital.com>
2024-01-07 15:33:28 +0530

    pages

--- resources/views/home/includes/header.blade.php
+++ resources/views/home/includes/header.blade.php
@@ -2,26 +2,28 @@
 
     <!--//to-bar-->
     <div class="header-main container">
-        <div class="info col-md-2 col-sm-2 " style="margin-top: 10px;">
+        <div class="info col-2" style="margin-top: 10px;">
             <div class="pull-left ">
                 <a href="#">
-                    <img src="{{ asset('frontend/assets/images/icon/lnmulogo.png') }}" alt="College Logo" width="130"
+                    <img src="{{ asset('frontend/assets/images/icon/lnmulogo.png') }}" alt="College Logo" style="max-width: 130px;"
                         class="img-responsive " />
                 </a>
             </div>
         </div>
-        <div class="info col-md-8 col-sm-8 text-center">
+        <div class="info col-md-8 col-8 text-center">
 
+            <img src="{{ asset('frontend/assets/images/animate-logo.gif') }}"  alt="NAAC Grade"
+                        class="img-responsive" width="700" style="float: right;" />
 
-            <div class="text-effect">
+            {{-- <div class="text-effect">
                 <span>R</span><span>A</span><span>M</span>
                 <span>K</span><span>R</span><span>I</span><span>S</span><span>H</span><span>N</span><span>A</span>
                 <span>C</span><span>O</span><span>L</span><span>L</span><span>E</span><span>G</span><span>E</span>
-            </div>
+            </div> --}}
 
             {{-- <h1 class="" style="font-weight: bold;color:#fa8303;letter-spacing: 3px;">RAM KRISHNA
                 COLLEGE</h1> --}}
-                <h4 class="" style="font-weight: bold; text-align: center;"> ( Madhubani, Bihar )</h4>
+                <h4 class="" style="font-weight: bold; text-align: center;">  Madhubani, Bihar </h4>
 
             <h5 class="" style="font-weight: bold; text-align: center;"> ( A Post Graduate Constituent Unit of
                 <a href="http://lnmu.ac.in/" style="color: #7C0000;">Lalit Narayana Mithila University,Darbhanga</a> )
